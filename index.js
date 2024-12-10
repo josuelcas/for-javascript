f89f9ad6728292810ffdf8a8af7ba68f22a4b2d9
josuelcas <rerelucas608@gmail.com>
2024-12-10 15:55:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+for(let j = 1; j < 10; j++){
+    var tabuada = j;
+  
+    for(var i =0; i <=10; i++){
+      console.log(tabuada + " X " + i + " = " + ( tabuada * i));
+    }
+  
+    console.log("");
+}
